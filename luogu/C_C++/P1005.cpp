25a10e78264e9b19c9c50fb97ae5ed70be286c43
謝旻言 <78300584+ax0080@users.noreply.github.com>
2023-11-27 11:18:51 +0800

    Update P1005.cpp

--- luogu/C_C++/P1005.cpp
+++ luogu/C_C++/P1005.cpp
@@ -12,120 +12,96 @@ dp[i, j] = 2 * max(dp[i+1, j] + data[i], dp[i, j-1] + data[j])
 */
 
 #include <iostream>
+#include <vector>
 #include <stdio.h>
 #include <algorithm>
 
-long long p=1e18; //long long範圍
+using namespace std;
 
-struct int128
-{
+long long p = 1e18;
+
+struct int128 {
     long long hig;
     long long low;
 };
 
-int128 max(int128 a,int128 b)
-{
-	if(a.hig>b.hig) return a;
-	if(a.hig<b.hig) return b;
-	if(a.low>b.low) return a;
-	if(a.low<b.low) return b;
-	return a;
+vector<vector<int128>> memo;  // 記憶化表格
+
+int128 max(const int128& a, const int128& b) {
+    if (a.hig > b.hig || (a.hig == b.hig && a.low > b.low)) return a;
+    return b;
 }
-int128 operator + (int128 a,int128 b)
-{
+
+int128 operator+(const int128& a, const int128& b) {
     int128 k;
-    k.low=0,k.hig=0;
-    k.low=a.low+b.low;
-    k.hig=k.low/p+a.hig+b.hig;
-    k.low%=p;
+    k.low = a.low + b.low;
+    k.hig = k.low / p + a.hig + b.hig;
+    k.low %= p;
     return k;
 }
-int128 operator * (int128 a,int b)
-{
-	int128 k;
-	k.low=0,k.hig=0;
-	k.low=a.low*b;
-	k.hig+=k.low/p+b*a.hig;
-	k.low%=p;
-	return k;
+
+int128 operator*(const int128& a, int b) {
+    int128 k;
+    k.low = a.low * b;
+    k.hig = k.low / p + a.hig * b;
+    k.low %= p;
+    return k;
 }
 
-int128 dp_sum(int front, int back, long long** matrix, int row, int128 sum);
+int128 dp_sum(int front, int back, const vector<vector<long long>>& matrix, int row, int128 sum);
 
 int main() {
+    int n, m;
 
-  long long **matrix;
-  int n, m;
+    scanf("%d %d", &n, &m);
+    vector<vector<long long>> matrix(n, vector<long long>(m));
 
-  scanf("%d %d", &n, &m);
-  matrix = new long long *[n];
-  for (int i = 0; i < n; i++) {
-    matrix[i] = new long long[m];
-  }
-
-  int head;
-  int tail;
-  int128 sum;
-  sum.low = 0;
-  sum.hig = 0;
-
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) 
-    {
-      scanf("%lld", &matrix[i][j]);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%lld", &matrix[i][j]);
+        }
     }
-  }
 
-  for (int i = 0; i < n; i++)
-  {
-    head = 0;
-    tail = m - 1;
-    int128 sum_row;
-    sum_row.hig = 0;
-    sum_row.low = 0;
-    sum = sum + dp_sum(head, tail, matrix, i, sum_row);
-  }
+    int128 sum;
+    sum.low = 0;
+    sum.hig = 0;
 
+    for (int i = 0; i < n; i++) {
+        int128 sum_row;
+        sum_row.low = 0;
+        sum_row.hig = 0;
+        sum = sum + dp_sum(0, m - 1, matrix, i, sum_row);
+    }
 
-  
-  if(sum.hig==0)	printf("%lld",sum.low);
-  else	printf("%lld%018lld\n",sum.hig,sum.low);
+    if (sum.hig == 0)
+        printf("%lld", sum.low);
+    else
+        printf("%lld%018lld\n", sum.hig, sum.low);
 
-
-  for (int i = 0; i < n; i++) 
-  {
-    delete[] matrix[i];
-  }
-  delete[] matrix;
-
-  //printf("finish\n");
-
-  return 0;
+    return 0;
 }
 
-int128 dp_sum(int front, int back, long long** matrix, int row, int128 sum) 
-{
-    int front_dp = front;
-    int back_dp = back;
-    
-
-    if(front_dp == back_dp) 
-    {
-      int128 temp;
-      temp.hig = 0;
-      temp.low = 2*matrix[row][front_dp]; 
-      return temp;
+int128 dp_sum(int front, int back, const vector<vector<long long>>& matrix, int row, int128 sum) {
+    if (front == back) {
+        int128 temp;
+        temp.hig = 0;
+        temp.low = 2 * matrix[row][front];
+        return temp;
     }
-    else 
-    {
-      int128 front_first;
-      int128 back_first;
-      front_first.hig = 0;
-      back_first.hig = 0;
-      front_first.low = matrix[row][front_dp];
-      back_first.low = matrix[row][back_dp];
 
-      return max((front_first + dp_sum(front_dp+1, back_dp, matrix, row, sum))*2, (back_first + dp_sum(front_dp, back_dp-1, matrix, row, sum))*2);
+    if (memo[front][back].low != -1) {
+        return memo[front][back];
     }
+
+    int128 front_first;
+    int128 back_first;
+    front_first.hig = 0;
+    back_first.hig = 0;
+    front_first.low = matrix[row][front];
+    back_first.low = matrix[row][back];
+
+    memo[front][back] = max((front_first + dp_sum(front + 1, back, matrix, row, sum)) * 2,
+                            (back_first + dp_sum(front, back - 1, matrix, row, sum)) * 2);
+
+    return memo[front][back];
 }
